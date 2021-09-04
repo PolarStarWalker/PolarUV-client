c15aad4b32328cdf90491407c5af859f09cca317
motov.s <motov.s@edu.narfu.ru>
2021-09-04 20:36:32 +0300

    ИНТЕРПРИТАТОР ПИТОНА!

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -3,6 +3,13 @@
 
 int main(int argc, char *argv[]) {
 
+
+    Py_Initialize();
+
+    PyRun_SimpleString("print('Hello!')");
+
+    Py_Finalize();
+
     SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
 
     QApplication a(argc, argv);
